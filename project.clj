4188d9f177d39ac8a620597cec0576d5ed269fa5
Jonathan Millett <jon@sistemimoderni.com>
2011-02-21 13:24:20 -0300

    Added lein ring integration.

--- project.clj
+++ project.clj
@@ -1,4 +1,11 @@
 (defproject sistemi "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Sistemi Moderni Website"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [ring/ring-jetty-adapter "0.3.6"]
+		 [compojure "0.6.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+		     [lein-ring "0.3.2"]]
+  :ring {:handler sistemi.core/main-routes})
+
+;; ? does "lein ring server" start a swank server as well?
--- src/sistemi/core.clj
+++ src/sistemi/core.clj
@@ -1,1 +1,20 @@
-(ns sistemi.core)
+; --------------------------------------------------
+; Main file for clojure Sistemi Moderni web application.
+
+; --------------------------------------------------
+; QUESTIONS
+; 
+; - Is the stacktrace useful?  (:use ring.middleware.stacktrace)
+
+(ns sistemi.core
+  (:use compojure.core
+	ring.adapter.jetty)
+  (:require [compojure.route :as route])
+  (:require [swank.swank]))
+
+(defroutes main-routes
+  (GET "/" [] "<h1>Hello Sistemi Moderni World!</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
+
+;; Start swank repl.
+(swank.swank/start-repl)
