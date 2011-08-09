7005a7b961eae41d6505af2fc730f56fd95c9d2e
Jonathan Millett <jon@sistemimoderni.com>
2011-08-08 23:58:59 -0400

    Switched logging to use clj-logging-config library.

--- project.clj
+++ project.clj
@@ -1,18 +1,25 @@
 ;; Dependency versions last checked on 7/27/11.
 (defproject sistemi "0.1"
   :description "Sistemi Moderni website"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 ; logging
-                 [org.clojure/tools.logging "0.1.2"]
-                 [org.slf4j/slf4j-api "1.6.1"]
-                 [ch.qos.logback/logback-classic "0.9.29"]
-                 ; web
-                 [ring/ring-core "0.3.11"]
-                 [ring/ring-devel "0.3.11"]
-                 [ring/ring-jetty-adapter "0.3.11"]
-                 [net.cgrand/moustache "1.0.0"]
-                 [enlive "1.0.0"]
-                 ; database
-                 [postgresql/postgresql "8.4-702.jdbc4"]
-                 [org.clojure/java.jdbc "0.0.5"]])
+  :dependencies[
+                ;; clojure
+                [org.clojure/clojure "1.2.1"]
+                [org.clojure/clojure-contrib "1.2.0"]
+
+                ;; logging
+                [org.clojure/tools.logging "0.1.2"]
+                [clj-logging-config "1.5"]
+                [org.slf4j/slf4j-api "1.6.1"]
+                [org.slf4j/slf4j-log4j12 "1.6.1"]
+                [log4j "1.2.16"]
+                
+                ;; web
+                [ring/ring-core "0.3.11"]
+                [ring/ring-devel "0.3.11"]
+                [ring/ring-jetty-adapter "0.3.11"]
+                [net.cgrand/moustache "1.0.0"]
+                [enlive "1.0.0"]
+
+                ;; database
+                [postgresql/postgresql "8.4-702.jdbc4"]
+                [org.clojure/java.jdbc "0.0.5"]])
--- src/sistemi/core.clj
+++ src/sistemi/core.clj
@@ -6,62 +6,37 @@
         ring.middleware.file-info
         ring.middleware.stacktrace
         [ring.adapter.jetty :only (run-jetty)]
-        [clojure.tools.logging :only (info error)]))
+        [clojure.tools.logging :only (info error)]
+        clj-logging-config.log4j))
 
+;;; ===== LOGGING =====
+(set-loggers!
+ :root {:level :info :pattern "%p|%c|%m%n"}
+ "sistemi" {:level :debug})
+
+;;; ===== RING HANDLERS =====
 (def handlers (app
                wrap-stacktrace
-;    (wrap-reload '[adder.middleware adder.core])
-;    (wrap-request-logging)
+               ;; (wrap-reload '[adder.middleware adder.core])
                wrap-file-info
                (wrap-file "www")
                ["sailing"] "clear sailing"
-               ; todo: add a custom 404 here
+               ;; TODO: add a custom 404 here
                ))
 
-
-;; (run-jetty #'handlers {:port 8080 :join? false})
-
-;; Starts a swank server. Useful when running locally from foreman.
+;;; ===== SWANK =====
+;;; Starts a swank server. Useful when running locally from foreman.
 (defn start-swank []
   (info "Starting swank.")
-  ; Use eval here since swank-clojure may not be available in production (e.g., heroku).
+  ;; Use eval here since swank-clojure may not be available in production (e.g., heroku).
   (eval '(do (require 'swank.swank)
              (swank.swank/start-server :host "localhost" :port 4005))))
 
+;;; ===== MAIN =====
 (defn -main []
-  ; Start swank if the SWANK environment variable is defined.
+  ;; Start swank if the SWANK environment variable is defined.
   (if (get (System/getenv) "SWANK") (start-swank))
-  ; Start jetty.
+  ;; Start jetty.
+  ;; TODO: What is the peformance impact of passing a var here?
   (let [port (Integer/parseInt (System/getenv "PORT"))]
     (run-jetty #'handlers {:port port})))
-
-;; - Update the format to work with foreman.
-;;   12:44:03.657 [main] INFO  sistemi.core - Starting swank.
-;; - Set default log level to INFO.
-
-
-;; ? did the original app use a var?
-;;   - use a var in dev mode?
-;;   ? what is the performance impact of using a var?
-
-;; SNIPPETS
-;; (handlers {:request-method :get :uri "/"})
-
-;; - get request logging working
-;;     - http://devcenter.heroku.com/articles/logging#writing_to_your_log
-;;     - http://stackoverflow.com/questions/2671454/heroku-how-to-see-all-the-logs
-;;     ? remote syslog server?
-;; - get reloading working in dev environment
-;; - get working on heroku
-;; - checkin to github
-;; - get a dev environment working for copeland
-;;   - http://stackoverflow.com/questions/5939878/git-make-development-and-master-track-different-repos-sensible
-;; - review lisa's document and identify server side tasks
-;; - paypal integration
-;; - enable SSL (www and naked domain)
-;; - design dev workflow
-;;   - http://devcenter.heroku.com/articles/git
-;;   - www.sistemimoderni.com
-;;   - test.sistemimoderni.com
-;;   - dev.sistemimoderni.com
-;; - get CI server working
