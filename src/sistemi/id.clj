e74bcd0e495d5e10d26fa93f912498330da09086
Jonathan Millett <jon@sistemimoderni.com>
2011-08-12 20:05:57 -0400

    Added functions to generate globally unique request identifiers (id.clj).

---
+++ src/sistemi/id.clj
@@ -0,0 +1,32 @@
+(ns sistemi.id
+  (:require [sistemi.base62 :as b62]))
+
+;; Generate a human readable reasonable unique request identifer for use in log messages.
+;;
+;; REQUIREMENTS
+;; - identifies a web request all requests and jvm instances within the last 30 days
+;; - identifies the jvm instance where the request arrived
+;; - human readable
+;; - short
+;; - collisions can happen but should be rare
+;;   - manage collisions by checking:
+;;     a) the timestamp on the log message
+;;     b) the foreman id (e.g., web(1))
+;;
+;; IMPLEMENTATION
+;; - Generate a boot identifier (4 chars base62 random string) at jvm startup to identify the jvm
+;;   instance.
+;; - Increment a per jvm request counter to identify each request within a jvm instance.
+;; - Generate a request id of the form bbbb.rrr where bbbb is the boot
+;;   identifier and rrr is the request sequence in base62.
+
+;; Generate a unique boot identifier when jvm starts.
+(def boot-id (b62/rand 4))
+
+;; Count requests from zero.
+(def request-count (atom 0))
+
+(defn next!
+  "Generates and returns a semi-unique request id."
+  []
+  (str boot-id "." (b62/encode (swap! request-count inc))))
