46cdd2427c54655af634d8a11694c51ac4682b74
Jonathan Millett <jon@sistemimoderni.com>
2011-09-01 19:58:28 -0300

    Added check and swallow macros to except.clj.

--- src/util/except.clj
+++ src/util/except.clj
@@ -1,6 +1,6 @@
 (ns util.except
   "Macros for handling and throwing exceptions."
-  (:use clojure.contrib.strint))
+  (:use [clojure.contrib.pprint :only (cl-format)]))
 
 (defmacro safely
   "Evaluates an expression safely by catching any exceptions thrown.
@@ -8,14 +8,68 @@
   [expr fallback]
   `(try ~expr (catch Exception x# ~fallback)))
 
-(defmacro die
-  "Logs an error message and throws an exception. String interpolation is applied to the error message."
-  [message]
-  `(let [s# (<< ~message)]
-     (log/error s#)
-     (throw (RuntimeException. s#))))
+(defmacro swallow
+  "Evaluates an expression swallowing any exceptions. Returns the result of evaluating expr or the
+   exception if one occurred."
+  [expr]
+  `(try ~expr (catch Exception x# x#)))
 
-(defmacro affirm
-  "Executes text-expr and returns the result if true. Otherwise calls die with the given message."
-  [test-expr message]
-  `(or ~test-expr (die ~message)))
+(defn die
+  "Throws a RuntimeException with the given message. When multiple arguments are passed, cl-format
+  is used to build the message string."
+  ([message]
+     (throw (RuntimeException. message)))
+  ([format-string & format-args]
+     (die (apply cl-format nil format-string format-args))))
+
+(defmacro check
+  "Evaluates expr and validates the result. The result is returned if validation succeeds. Otherwise, an
+   exception is thrown.
+
+   A custom validator function can be passed. If not passed, the function identity will be used to
+   test for logical truth of the result.
+
+   A custom error message format and optional format arguments may be passed. The error message is
+   constructed using the cl-format function with the supplied format and args. During formatting,
+   the offending result value will be inserted as the first format arg.
+
+   EXAMPLES
+
+   ;; Simple message.
+   (swallow (check :zz #{:en :es :de :fr} \"I don't know that language.\"))
+   => #<RuntimeException java.lang.RuntimeException: I don't know that language.>
+
+   ;; Format string that includes the offending value.
+   (swallow (check :zz #{:en :es :de :fr} \"I don't speak '~A'. Are you asleep?\"))
+   => #<RuntimeException java.lang.RuntimeException: I don't speak ':zz'. Are you asleep?>
+
+   ;; Format string with additional arguments. Note that the offending value is inserted as the
+   ;; first format argument.
+   (let [langs #{:en :es :de :fr}]
+     (swallow (check :zz langs \"I don't speak '~A'. I do speak ~{~a~^, ~}.\" langs)))
+   => #<RuntimeException java.lang.RuntimeException: I don't speak ':zz'. I do speak :en, :es, :de, :fr.>
+
+   ;; If the validator function is omitted, logical truth will be tested.
+   (swallow (check nil \"Am I the eggman.\"))
+   => #<RuntimeException java.lang.RuntimeException: Am I the eggman.>
+
+   (swallow (check false \"Am I the eggman? ~A.\"))
+   => #<RuntimeException java.lang.RuntimeException: Am I the eggman? false.>
+
+   (swallow (check false \"Am I the eggman? ~A (~A).\" \"goo goo g'joob\"))
+   => #<RuntimeException java.lang.RuntimeException: Am I the eggman? false (goo goo g'joob).>
+"
+  ([expr]
+     `(check ~expr identity))
+  ([expr arg]
+     (if (string? arg)
+       `(check ~expr identity ~arg)
+       `(check ~expr ~arg "Check failed: invalid value '~A' for validator '~A'." '~arg)
+       ))
+  ([expr validate-fn format-string & format-args]
+     (if (string? validate-fn)
+       `(check ~expr identity ~validate-fn ~format-string ~@format-args)
+       `(let [result# ~expr]
+          (if (~validate-fn result#)
+            result#
+            (die ~format-string result# ~@format-args))))))
---
+++ test/util/test/except.clj
@@ -0,0 +1,42 @@
+(ns util.test.except
+  (:use util.except
+        clojure.test))
+
+(deftest test-safely
+  (are [x y] (= y (safely x :failed))
+       :worked              :worked
+       (throw (Exception.)) :failed
+       ))
+
+(deftest test-swallow
+  (is (= :worked (swallow :worked)))
+  (let [x (swallow (Exception. "fubar"))]
+    (is (instance? Exception x))
+    (is (= "fubar" (. x getMessage)))))
+
+(deftest test-die
+  (is (thrown-with-msg? RuntimeException #"bad news" (die "bad news")))
+  (is (thrown-with-msg? RuntimeException #"really bad news" (die "~A bad news" "really"))))
+
+(deftest test-check
+  (testing "one arg"
+    (is (= true (check true)))
+    (is (= "abc" (check "abc")))
+    (is (thrown-with-msg? RuntimeException #"Check failed:" (check false))))
+  (testing "custom validator"
+    (is (= 1 (check 1 pos?)))
+    (is (thrown-with-msg? RuntimeException #"Check failed:" (check 0 pos?))))
+  (testing "custom validator and message"
+    (is (= :de (check :de #{:en :es :de :fr} "I don't know that language.")))
+    (is (thrown-with-msg? RuntimeException #"I don't know that language." (check :zz #{:en :es :de :fr} "I don't know that language.")))
+    (is (thrown-with-msg? RuntimeException #"I don't speak ':zz'." (check :zz #{:en :es :de :fr} "I don't speak '~A'. Are you asleep?")))
+    (let [langs #{:en :es :de :fr}]
+      (is (thrown-with-msg? RuntimeException #"I don't speak ':zz'. I do speak :en, :es, :de, :fr." (check :zz #{:en :es :de :fr} "I don't speak '~A'. I do speak ~{~a~^, ~}." langs)))
+      ))
+  (testing "custom message"
+    (is (= 42 (check 42 "Apparently that is not the answer to life the universe and everything.")))
+    (is (thrown-with-msg? RuntimeException #"I am the eggman." (check nil "I am the eggman.")))
+    (is (thrown-with-msg? RuntimeException #"Am I the eggman\? false\."
+          (check false "Am I the eggman? ~A.")))
+    (is (thrown-with-msg? RuntimeException #"Am I the eggman\? false \(goo goo g'joob\)\."
+          (check false "Am I the eggman? ~A (~A)." "goo goo g'joob")))))
