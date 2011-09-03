c2efe3e83949161ff966a0647c700f45af681de2
Jonathan Millett <jon@sistemimoderni.com>
2011-09-03 10:43:47 -0300

    Added enhancements to run_level.clj: decouple from conf, custom run levels, and associated tests.

--- src/app/run_level.clj
+++ src/app/run_level.clj
@@ -1,30 +1,71 @@
 (ns app.run-level
-  (:require [clojure.tools.logging :as log])
-  (:use [app.config :only (*config*)]
-        [util.except :only (affirm)]
-        [clojure.contrib.def :only (defvar)]))
+  (:use [clojure.contrib.def :only (defvar)]
+        util.except))
 
-(defn- get-run-level
-  "Looks up the application run level from the environment variable RUN_LEVEL. If not found, the
-   level :development is returned."
+;; ===== VARS =====
+
+(defvar run-levels
+  "Set of valid run levels."
+  nil)
+
+(defvar run-level
+  "Runtime level of the process."  
+  nil)
+
+;; ===== FUNCTIONS =====
+
+(defn- make-predicates
+  "Generates predicate functions for testing the run-level."
   []
-  (let [level (keyword (:run-level *config* :development))]
-    (affirm (level #{:development :staging :production}) "Unknown run level '~{level}'.")))
+  (doseq [level run-levels]
+    (let [fn-sym (symbol (str (name level) "?"))
+          fn-var (intern 'app.run-level fn-sym (fn [] (= run-level level)))]
+      (alter-meta! fn-var assoc :doc (str "Returns true if the run level is " level ".")))))
 
-(defvar *run-level* (get-run-level)
-  "Runtime level of the process.")
+(defn init-run-level!
+  "Initializes the current run level and the allowed set of run levels.
 
-(defn development?
-  "Returns true if the run level is development."
-  []
-  (= :development *run-level*))
+   Set of Run Levels
+   The set of allowed run levels can be passed in a vector of keywords.
+   If not specified, it defaults to #{:development :staging :production}.
 
-(defn staging?
-  "Returns true if the run level is staging."
-  []
-  (= :staging *run-level*))
+   Run Level
+   The active run level can be passed as a keyword. If not passed,
+   it is set from the environment variable RUN_LEVEL or to :development if
+   RUN_LEVEL is not defined.
 
-(defn production?
-  "Returns true if the run level is production."
-  []
-  (= :production *run-level*))
+   Predicate Functions
+   For each run level, a predicate function is generated which returns
+   true if that run level is the current one. E.g., the predicate
+   development? will be generated from the run level :development.
+
+   EXAMPLES
+   ;; SIMPLE
+   ;; Initialize run-levels to #{:development :staging :production} and
+   ;; run-level to the value of the RUN_LEVEL environment variable.
+   (init-run-level!)
+
+   ;; SET RUN LEVEL
+   ;; Initialize run-level to :production and run-levels to #{:development :staging :production}.
+   (init-run-level! :production)
+
+   ;; SET CUSTOM RUN LEVELS
+   ;; Initialize run-levels to #{:tinkering :sales-demo :testing :production} and
+   ;; run-level to the value of the RUN_LEVEL environment variable.
+   (init-run-level! #{:tinkering :sales-demo :testing :production})
+
+   ;; Set CUSTOM RUN LEVELS
+   ;; Initialize run-levels to #{:tinkering :sales-demo :testing :production} and
+   ;; run-level to :sales-demo.
+   (init-run-level! :sales-demo #{:tinkering :sales-demo :testing :production})
+"
+  ([] (init-run-level! nil))
+  ([arg] (if (keyword? arg)
+           (init-run-level! arg nil)
+           (init-run-level! nil arg)))
+  ([level levels]
+     (let [levels (or levels #{:development :staging :production})
+           level (or level (keyword (. (System/getenv) get "RUN_LEVEL")) :development)]
+       (alter-var-root #'run-levels (constantly (set (check levels set?))))
+       (alter-var-root #'run-level (constantly (check level levels))))
+     (make-predicates)))
---
+++ test/app/test/run_level.clj
@@ -0,0 +1,36 @@
+(ns app.test.run-level
+  (:use clojure.test
+        app.run-level))
+
+(defn- resolve-predicate
+  "Takes a level keyword and returns the var for its predicate function."
+  [level]
+  (ns-resolve 'app.run-level (symbol (str (name level) "?"))))
+
+(deftest test-init-run-level!
+  (testing "no args"
+    (init-run-level!)
+    (is (= run-level :development))
+    (is ((resolve-predicate :development)))
+    (is (not ((resolve-predicate :staging))))
+    (is (not ((resolve-predicate :production)))))
+  (testing "one arg: level"
+    (init-run-level! :staging)
+    (is (= run-level :staging))
+    (is ((resolve-predicate :staging))))
+  (testing "one arg: levels"
+    (init-run-level! #{:development :office :demo :live})
+    (is (= run-level :development))
+    (is (not ((resolve-predicate :office)))))
+  (testing "two args"
+    (init-run-level! :bar #{:foo :bar :baz})
+    (is (= run-level :bar))
+    (is ((resolve-predicate :bar)))
+    (is (thrown? RuntimeException #"Check failed" (init-run-level! :quux #{:foo :bar :baz})))
+    (is (thrown? RuntimeException #"Check failed" (init-run-level! :a "a b c"))))
+  (testing "predicates"
+    (are [level] (re-find (re-pattern (str "run level is " level))
+                          (:doc (meta (resolve-predicate level))))
+         :foo
+         :bar
+         :baz)))
