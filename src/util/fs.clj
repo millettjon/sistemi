6aac5b11329c55ce512e2f1ba34afb522ff25c7a
Jonathan Millett <jon@sistemimoderni.com>
2011-09-15 16:52:39 -0300

    Updated fs.clj with additional functions, notes, and tests.

--- src/util/fs.clj
+++ src/util/fs.clj
@@ -1,6 +1,59 @@
 (ns util.fs
   "Utilities for manipulating / separated paths."
-  (:use [clojure.contrib.string :only (split replace-re)]))
+  (:require [clojure.contrib.io :as io])
+  (:use [clojure.contrib.string :only (split replace-re)])
+  (:import java.io.File))
+
+;; GOALS
+;; - easy manipulations of paths
+;; - don't use java.io.File since it is superflous
+;; - clojurization
+;;   - use sequences
+;;   - use predicates
+;;   - follow idioms
+;; - use / as path separator
+;;   - coerce internally where necessary for platform operations
+;; - encapsulate java interop
+;; - batteres included
+;;   - typical shell operations
+;; - review clojure/java/ruby/python/shell for others
+;; - handle both file and url paths
+;; FUNCTIONS
+;; - is fs (file segment/file system) the right name? (file?)
+;; - child?         ; true if a is child of p
+;; - empty?         ; true if dir has no children or file size is zero
+;; - exists?        ; true if path exists?
+;; - root?          ; true if path is "/"
+;; - symlink?       ; true if path is a symlink
+;; - readlink       ; true if path is a 
+;; - dir?           ; true if path is a directory
+;; - file?          ; true if path is a file
+;; - children       ; seq of children; breadth-first; default depth is 1 (children "/foo" 1)
+;; - dirs           ; seq of child directories
+;; - files          ; seq of child files
+;; - parents        ; seq of parents
+;; - breadth-first/ ; macro that sets a binding to make tree seq order breadth first (breadth-first children "/tmp")
+;;   order                                                                           (order :breadth-fist expr)
+;; - as-unix        ; coerce from dos to unix format
+;; - as-dos         ; coerce from unix to dos format
+;; - as-local       ; coerce to format of local platform
+;; - parent         ; parent path
+;; - name           ; file or dir name
+;; - extension      ; file extension
+;; - parts          ; seq of path segements
+;; - canonicalize   ; returns the canonical path
+;; - absolute?      ; true if path is absolute
+;; - relative?      ; true if path is relative
+;; - qualify        ; makes path absolute if not already (using ~ or supplied or current dir)
+;; - unqualify      ; makes path relative by removing the supplied prefix (or number of parts)
+;; - expand/glob/find    ; returns seq of matching shell expansions (including **/..)
+;; MISC
+;; - move, copy, delete, touch, temp-file, delete, chmod, chown, extended attributes, get and set times
+
+;; namespace -> fs, file (file/shell)
+;; fs -> file, join
+;; fs-seq -> parts, split
+
 
 (defn fs
   "Joins path segments. Condenses consecutive slashes into a single slash.
@@ -24,14 +77,65 @@
      (= \/ (first path)) path
      :default (str "/" path))))
 
+(defn relative?
+  "Returns true if the path is a relative path."
+  [path]
+  (cond
+   (nil? path) (throw (NullPointerException.))
+   (empty? path) true
+   :default (not= \/ (first path))))
+
 (defn fs-seq
  "Takes a list of paths splits them into segments and returns the flattened seq."
  ([] nil)
- ([arg & args]
-    (flatten (map #(split #"/" %1) (concat (list arg) args)))))
+ ([& args]
+    (let [args (filter #(not (empty? %1)) args)]
+      (seq (filter #(not (empty? %1))
+                   (flatten
+                    (map #(split #"/" %1) args)))))))
+
+(defn fs-rest
+  "Removes the first path segment."
+  [path]
+  (if (nil? path)
+      nil
+      (apply (if (relative? path) fs ffs) (rest (fs-seq path)))))
+
+;; TODO: Remove 0 arity unless needed (e.g., for apply).
+;; TODO: See if type hints are necessary.
 
 (defn parent
   "Returns the parent path of a path or nil if there is no parent."
   ([] nil)
   ([path]
-  (apply fs (butlast (fs-seq path)))))
+     (let [seq (butlast (fs-seq path))] 
+          (if (and (empty? seq) (> (count path) 1) (= \/ (nth path 0)) )
+            "/"
+            (apply fs (butlast (fs-seq path)))))))
+
+(defn root?
+  "Returns true if the path is the root path i.e., \"/\"."
+  ([] nil)
+  ([path]
+  (= "/" path)))
+
+(defn canonical
+  "Returns the canonical form of a path."
+  [path]
+  (.getPath (.getCanonicalFile (File. path))))
+
+(defn file-seq-bf [dir]
+  "Returns a seq of all files and subdirectories of a directory in breadth first order."
+  (let [dir (io/file dir)
+        children (.listFiles dir)
+        subdirs (filter #(.isDirectory %) children)
+        files (filter #(.isFile %) children)]
+    (concat [dir] files (mapcat file-seq-bf subdirs))))
+
+(defn dir-seq-bf
+  "Returns a seq of all subdirectories of a directory in breadth first order."
+  [dir]
+  (let [dir (io/file dir)
+        children (.listFiles dir)
+        subdirs (filter #(.isDirectory %) children)]
+    (concat [dir] (mapcat dir-seq-bf subdirs))))
--- test/util/test/fs.clj
+++ test/util/test/fs.clj
@@ -29,6 +29,10 @@
 (deftest test-fs-seq
   (are [a b] (= (apply fs-seq a) b)
        []           nil
+       [nil]        nil
+       [""]         nil
+       ["/"]        nil
+       ["/a"]       ["a"]
        ["a"]        ["a"]
        ["a/b"]      ["a" "b"]
        ["a/b" "c"]  ["a" "b" "c"]
@@ -37,7 +41,33 @@
 (deftest test-parent
   (are [a b] (= (apply parent a) b)
        []           nil
+       [""]         nil
+       ["/a"]       "/"
        ["a"]        nil
        ["a/b"]      "a"
        ["a/b/c"]   "a/b"
        ))
+
+(deftest test-relative?
+  (are [a b] (= (relative? a) b)
+       ""        true
+       "abc"     true
+       "abc/def" true
+       "/a"      false
+       "/a/b"    false
+       "/"       false
+       )
+  (is (thrown? NullPointerException (relative? nil))))
+
+(deftest test-fs-rest
+  (are [a b] (= (fs-rest a) b)
+       nil     nil
+       ""      nil
+       "/"     nil
+       "/a"    nil
+       "a"     nil
+       "/a/b"  "/b"
+       "/a/b/" "/b"
+       "a/b"   "b"
+       )
+  )
