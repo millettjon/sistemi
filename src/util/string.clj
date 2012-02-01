a37f3687d69708218956783c9a68a7596e3c4aff
Jonathan Millett <jon@sistemimoderni.com>
2012-02-01 06:32:10 -0300

    Created util.string/join-lines and triml-lines to join and trim multi-line strings.

--- src/util/string.clj
+++ src/util/string.clj
@@ -1,6 +1,17 @@
-(ns util.string)
+(ns util.string
+  (require [clojure.string :as str]))
 
 (defn starts-with?
   "Returns true if string starts with substring."
   [string substring]
   (.startsWith string substring))
+
+(defn triml-lines
+  "Deletes leading whitespace from a multi-line string."
+  [s]
+  (str/join "\n" (map str/triml (str/split-lines s))))
+
+(defn join-lines
+  "Converts new lines to spaces and deletes leading whitespace from a multi-line string."
+  [s]
+  (str/join " " (map str/triml (str/split-lines s))))
---
+++ test/util/test/string.clj
@@ -0,0 +1,15 @@
+(ns util.test.string
+  (:use clojure.test
+        util.string))
+
+(deftest triml-lines-test
+  (are [a b] (= (triml-lines a) b)
+       "a" "a"
+       "a\nb" "a\nb"
+       "  a\n    b" "a\nb"))
+
+(deftest join-lines-test
+  (are [a b] (= (join-lines a) b)
+       "a" "a"
+       "a\nb" "a b"
+       "  a\n    b" "a b"))
