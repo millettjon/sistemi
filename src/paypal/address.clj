b2ad5e169021d89d9e1fbc675e5f356927da5558
Jonathan Millett <jon@sistemimoderni.com>
2012-06-24 18:42:40 -0400

    Convert addresses from paypal to upu format.

---
+++ src/paypal/address.clj
@@ -0,0 +1,39 @@
+(ns paypal.address
+  "Convert paypal addresses to UPU addresses."
+  (:require upu))
+
+(defn- concat-keyword
+  "Concats arguments into a single keyword."
+  [& args]
+  (keyword (apply str (map name args))))
+
+(defn- GB
+  [m prefix]
+  {:name (m (concat-keyword prefix :name))
+   :street (m (concat-keyword prefix :street))
+   :city (m (concat-keyword prefix :city))
+   :county (m (concat-keyword prefix :state))
+   :code (m (concat-keyword prefix :zip))
+   :country "GB"})
+
+(defn to-upu
+  "Converts an address from paypal to upu format."
+  [m prefix]
+  ((ns-resolve 'paypal.address
+               (symbol (name ((concat-keyword prefix :countrycode) m)))) m prefix))
+
+(defn format
+  "Converts an address to upu format and then calls upu/format."
+  [m prefix]
+  (upu/format (to-upu m prefix)))
+
+#_(let [m {:shiptoname "Jonathan Millett",
+         :shiptostreet "1 Main Terrace",
+         :shiptocity "Wolverhampton",
+         :shiptostate "West Midlands", ; is a county
+         :shiptozip "W12 4LQ",
+         :shiptocountryname "United Kingdom",
+         :shiptocountrycode "GB"}]
+  #_(to-upu m :shipto)
+  (format m :shipto)
+  )
