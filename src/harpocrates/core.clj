7a44396a1c6c731936800c09bc30db46aa1253cd
Jonathan Millett <jon@sistemimoderni.com>
2012-10-02 23:42:57 -0300

    Removed excess comments related to gpg.

--- src/harpocrates/core.clj
+++ src/harpocrates/core.clj
@@ -23,35 +23,11 @@
     ;; only apply to maps
     (postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
 
-
-;; GPG SETUP INSTRUCTIONS
-;;
-;; Generate a key.
-;;   gpg --home=.gnupg --gen-key
-;; 
-;; List keys.
-;;   gpg --home=.gnupg --list-keys
-;;
-;; Encrypt a file.
-;;   gpg --home=.gnupg --encrypt -r gpg-dev@sistemimoderni.com credentials.clj  # --> credentials.clj.gpg
-;;
-;; Decrypt a file.
-;;   gpg --home=.gnupg --decrypt credentials.clj.gpg
-;;
-;; Automated decryption via reading the passphrase from stdin.
-;;   echo 'xyzzy' | gpg --home=.gnupg --decrypt --passphrase-fd=0 --quiet --no-tty credentials.clj.gpg 2>/dev/null
-;;
-;;
-;; QUESTIONS
-;; ? What about simply using a long password instead of keys?
-;;   - no way to integrate w/ gpg agent
-
 (defn decrypt
   "Decrypts a file using gpg and returns the result.
    The passphrase can be supplied via the :passphrase option and will be passed to gpg on stdin.
    The gpg home directory can be overriden by passing the :home option.
-   The decrypted text is passed to read-string. Classify is then called to tag leaf literals as secret.
-"
+   The decrypted text is passed to read-string. Classify is then called to tag leaf literals as secret."
   [file & opts]
   (let [opts (merge {:passphrase nil, :home nil}
                     (apply hash-map opts))
