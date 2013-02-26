fe6ff96e440707ac9b9a2a1b48d826850057a4fb
Jonathan Millett <jon@sistemimoderni.com>
2013-02-26 09:35:19 -0300

    Updated dep ordered.

--- project.clj
+++ project.clj
@@ -51,7 +51,7 @@
 
                      ;; misc
                      [org.clojure/core.cache "0.6.2"]
-                     [ordered "1.2.0"]
+                     [ordered "1.3.2"]
                      [frinj "0.1.4"]
                      [com.google.guava/guava "13.0.1"]
                      [org.clojure/tools.namespace "0.2.2"]]
