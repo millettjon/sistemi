937b52f7eb796f48540ac59f6e84c8f70ae70187
Jonathan Millett <jon@sistemimoderni.com>
2013-02-26 09:22:43 -0300

    Refactored paypal/check-for-updates to be cleaner.

--- src/paypal/check_for_updates.clj
+++ src/paypal/check_for_updates.clj
@@ -2,30 +2,21 @@
   (:use paypal
         app.config))
 
-#_(defn -main
-  "Check that the live API versions for the sandbox and production sites match the configured ones."
+(defn- get-updates
+  "Returns a seq of updates available for a paypal sandbox and production sites."
   []
-  (doseq [site [:sandbox :production]]
-    (with-conf {:site site}
-      (let [expected (get-in paypal.config/site-conf [site :version])
-            actual (last (re-find #"^([\d\.]+)-" (get-version)))]
-        (when-not (= expected actual)
-          (with-bindings {#'*out* *err*}
-            (println (str "Warning: The PayPal " (name site) " API is at version " actual " but the code is at version " expected ".")))
-          (System/exit 1))))))
+  (filter identity (for [site [:sandbox :production]]
+                     (with-conf {:site site}
+                       (let [expected (get-in paypal.config/site-conf [site :version])
+                             actual (last (re-find #"^([\d\.]+)-" (get-version)))]
+                         (when-not (= expected actual)
+                           [site expected actual]))))))
 
 (defn -main
   "Check that the live API versions for the sandbox and production sites match the configured ones."
   []
-  (let [results (for [site [:sandbox :production]]
-                  (with-conf {:site site}
-                    (let [expected (get-in paypal.config/site-conf [site :version])
-                          actual (last (re-find #"^([\d\.]+)-" (get-version)))]
-                      (when-not (= expected actual)
-                        [site expected actual]))))
-        results (filter identity results)]
-    (when-not (empty? results)
-      (with-bindings {#'*out* *err*}
-        (doseq [[site expected actual] results]
-          (println (str "Warning: The PayPal " (name site) " API is at version " actual " but the code is at version " expected "."))))
-      (System/exit 1))))
+  (when-let [results (get-updates)]
+    (with-bindings {#'*out* *err*}
+      (doseq [[site expected actual] results]
+        (println (str "Warning: The PayPal " (name site) " API is at version " actual " but the code is at version " expected "."))))
+    (System/exit 1)))
