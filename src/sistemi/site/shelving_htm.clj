d2f44af7fbd2a19a505119582dcba6838e780a01
Jonathan Millett <jon@sistemimoderni.com>
2013-05-26 23:07:09 -0400

    shelving page - minor layout adjustments to background button per Einat.

--- src/sistemi/site/shelving_htm.clj
+++ src/sistemi/site/shelving_htm.clj
@@ -88,20 +88,17 @@
 
     ;; Toolbar
     ;; Note: This must be position: relative to allow the spin text to be positioned relative to it.
-    [:div {:style "height: 70px; position: relative;"}
+    [:div {:style "height: 50px; position: relative; margin-top: 20px;"}
 
      ;; Note: This must be positioned with z-index: 1 to be over the
      ;; spin div.
-     [:button#toggle-background.btn.btn-inverse.btn-small {:style "margin-left: 20px; position: relative; z-index: 1"}
+     [:button#toggle-background.btn.btn-inverse {:style "margin-left: 20px; position: relative; z-index: 1; outline: none;"}
       [:i.icon-white.icon-adjust {:style "margin-right: 10px;"}] "background"]
 
      [:div {:style "position: absolute; top: 5px; width: 100%; text-align: right; z-index: 0;"}
       [:span {:style "margin-right: 20px;"} (tr/translate :spin)]]
 
      ]]
-
-#_ [:button#submit.btn.btn-inverse {:type "submit" :tabindex 1} (if (= -1 (f/default :id)) (tr/translate :cart :add)
-                                                                      (tr/translate :cart :update))]
 
    [:div.span3
     
