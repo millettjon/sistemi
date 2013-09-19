bc40cab329b43ae2c1c11c86bf726723b1edffc5
David Millett <dbmillett@gmail.com>
2013-09-18 22:20:07 -0500

    Issue 7: RAL color wheel works, but 'add to cart' broken due to shelf.color = nil problem

--- src/sistemi/site/shelf_htm.clj
+++ src/sistemi/site/shelf_htm.clj
@@ -48,12 +48,14 @@
 
         ;; customSelect
         [:script {:type "text/javascript" :src "jquery.customSelect/customSelect.jquery.js"}]
-        [:style "span.customStyleSelectBox {height: 23px; line-height: 24px; width: 80px; background-color: #fff; color:#555; padding:0px 3px 0px 7px; border:1px solid #e7dab0; -moz-border-radius: 5px; -webkit-border-radius: 5px;border-radius: 5px 5px; }"]
+        [:style "span.customStyleSelectBox {height: 23px; line-height: 24px; width: 150px; background-color: #fff;
+                                            color:#555; padding:0px 3px 0px 5px; border:1px solid #e7dab0;
+                                            -moz-border-radius: 5px; -webkit-border-radius: 5px;
+                                            border-radius: 5px 5px; }"]
         [:style ".customStyleSelectBoxInner {background:url(jquery.customSelect/arrow.png) no-repeat center right; }"]
 
         ;; color picker
-        [:script {:type "text/javascript" :src "farbtastic/farbtastic.js"}]
-        [:link {:rel "stylesheet" :href "farbtastic/farbtastic.css" :type "text/css"}]
+        [:script {:type "text/javascript" :src "js/main.js"}]
 
         ;; 3d model
         [:script {:type "text/javascript" :src "/3d/Three.js"}]
@@ -78,7 +80,7 @@
 (defn body
   []
   [:div.row
-   [:div.span6
+   [:div#design-box.span6  {:style "background-color: #000;"}
     [:div#model]
 
     [:div {:style "text-align: center;"} (tr/translate :spin)]]
@@ -94,23 +96,36 @@
       [:div.control-group
        [:label.control-label {:for "width"} (tr/translate :width)]
        [:div.controls
-        (f/select :width {:class "chzn-select" :tabindex 1})]]
+        (f/select :width {:class "chzn-select" :style "width: 160px" :tabindex 1})]]
 
       [:div.control-group
        [:label.control-label {:for "depth"} (tr/translate :depth)]
        [:div.controls
-        (f/select :depth {:class "chzn-select" :tabindex 1})]]
+        (f/select :depth {:class "chzn-select" :style "width: 160px" :tabindex 1})]]
 
       [:div.control-group
        [:label.control-label {:for "finish"} (tr/translate :finish)]
        [:div.controls
-        (f/select :finish {:class "customStyleSelectBox" :style "width: 100px" :tabindex 1})]]
+        (f/select :finish {:class "customStyleSelectBox" :style "width: 165px" :tabindex 1})]]
 
       [:div.control-group
        [:label.control-label {:for "color"} (tr/translate :color)]
        [:div.controls
-        (f/text :color {:tabindex 1})]]
-      [:div#colorpicker {:style "margin-left: 20px;"}]
+        [:div {:style {:height "25px"}}
+         [:div#color-swatch {:style {:width "25px" :height "100%" :background-color "blue" :display "inline-block" :border-radius "5px" :vertical-align "middle"}}]
+         [:div#color-text {:style {:margin-left "10px" :display "inline-block" :height "100%" :vertical-align "middle" :padding-top "4px" :color "white"}}
+          "RAL 8098"
+          ]]]]
+
+      ;; TODO: Pull request for hiccup to handle style as map.
+      ;; TODO: Factor this out?
+      [:div {:style {:position :relative
+                     :height "195px"        ; manual height since children are absolute
+                     :margin-left "25px"}}  ; manual margin to right align wheels with other fields
+       [:div#wheel-ral {:style "position: absolute; top: 0px; left: 0px;"}]
+       [:div#wheel-val {:style "position: absolute; top: 0px; left: 0px; visibility: hidden;"}]
+       [:div#wheel-val-oiled {:style "position: absolute; top: 0px; left: 0px; visibility: hidden;"}]]
+
 
       [:div.control-group {:style "margin-top: 20px;"}
        [:label.control-label {:for "quantity"} (tr/translate :quantity)]
@@ -119,9 +134,10 @@
         [:div#numDisplayed {:style "text-align: center; visibility: hidden"} "(4 displayed)"]
         ]]
 
-      [:div {:style "text-align: right"}
+      [:div {:style "text-align: center"}
        [:button#submit.btn.btn-inverse {:type "submit" :tabindex 1} (if (= -1 (f/default :id)) (tr/translate :cart :add)
-                                                                      (tr/translate :cart :update) )]]]]
+                                                                      (tr/translate :cart :update) )]]
+      ] ]
 
     [:script {:type "text/javascript"}
      ;; Initialize shelf from defaults.
@@ -134,7 +150,6 @@
 
      "jQuery(document).ready(function() {
          // Hookup the form controls.
-         $('#colorpicker').farbtastic('#color');
          $('.chzn-select').chosen();
          $('.customStyleSelectBox').customSelect();
 
@@ -143,13 +158,107 @@
            shelf.width = $(this).val();
            updateAnimation(shelf);
          });
+
          $('#depth').chosen().change(function() {
            shelf.depth = $(this).val();
            updateAnimation(shelf);
          });
-         $('#quantity').chosen().change(function() {
-           shelf.quantity = $(this).val();
-           updateAnimation(shelf);
+
+         // Handler - When the finish is changed, update the color wheel.
+         var lastRAL;
+         var lastVAL;
+         $('#finish').change(function() {
+           var clr;
+           var finish = $(this).val();
+           $('#wheel-ral').css('visibility', 'hidden');
+           $('#wheel-val').css('visibility', 'hidden');
+           $('#wheel-val-oiled').css('visibility', 'hidden');
+           switch (finish) {
+             case ':laquer-matte':
+             case ':laquer-satin':
+               c = typeof lastRAL == 'undefined' ? defaultRAL : lastRAL;
+               onColor(c);
+               $('#wheel-ral').css('visibility', 'visible');
+               break;
+             case ':valchromat-raw':
+               c = typeof lastVAL == 'undefined' ? defaultVALraw : lastVAL;
+               console.log(c);
+               c = color.valchromat.get_by_name_js('raw', c.name);
+               onColor(c);
+               $('#wheel-val').css('visibility', 'visible');
+               break;
+             case ':valchromat-oiled':
+               c = typeof lastVAL == 'undefined' ? defaultVALoiled : lastVAL;
+               c = color.valchromat.get_by_name_js('oiled', c.name);
+               onColor(c);
+               $('#wheel-val-oiled').css('visibility', 'visible');
+           }
+
+         });
+
+         // --------------------
+         // Setup the color picker.
+         function onColor(color) {
+           pack = function(rgb) {
+             var r = rgb[0];
+             var g = rgb[1];
+             var b = rgb[2];
+             return '#' + (r < 16 ? '0' : '') + r.toString(16) +
+                          (g < 16 ? '0' : '') + g.toString(16) +
+                          (b < 16 ? '0' : '') + b.toString(16);
+           }
+
+           // TODO: Update hidden form field w/ EDN value.
+           var rgb;
+           var text;
+           var t = color.type;
+           if (t == 'ral') {
+             text = t.toUpperCase() + ' ' + color[t];
+             rgb = color.rgb;
+             lastRAL = color;
+           }
+           else {
+             lastVAL = color;
+             text = color.name;
+             rgb = pack(color.rgb);
+           }
+
+           // Update text of color select box.
+           $('#color-text').text(text);
+
+           // Update background color of color swatch.
+           // TODO: Use texture if available.
+           $('#color-swatch').css('background-color', rgb);
+
+           // Update the model.
+           var hex = rgbHexToInt(rgb);
+           if (shelf.color != hex) {
+             shelf.color = hex;
+             updateAnimation(shelf);
+           }
+         }
+
+         var defaultRAL = color.ral.default_color;
+         var defaultVALraw = color.valchromat.default_raw_color;
+         var defaultVALoiled = color.valchromat.default_oiled_color;
+
+         var callback = onColor;
+         color.ral_picker.init('#wheel-ral', color.ral.palette, callback);
+         color.val_picker.init('#wheel-val', 'raw', callback);
+         color.val_picker.init('#wheel-val-oiled', 'oiled', callback);
+         // --------------------
+
+         // Toggle background button handler.
+         $('#toggle-background').click(function(e) {
+           var box = $('#design-box');
+           var bg = box.css('background-color');
+           console.log(bg);
+           if (bg === 'rgb(255, 255, 255)') {
+             box.css('background-color', '#000');
+           }
+           else {
+             box.css('background-color', '#FFF');
+           }
          });
 
          // Set the container div height to match the width.
@@ -159,27 +268,58 @@
 
          // Start animating.
          drawShelf(shelf, model[0]);
+         onColor(defaultRAL);
          startAnimation();
-
-         // The color picker doesn't have a way to supply a callback in addition
-         // to the synched form field. So, periodically check the background color
-         // of the synched form field and re-render when it changes.
-         function checkColor() {
-           var color = $('#color').css('background-color');
-           color = rgb2hex(color);
-           if (shelf.color != color) {
-             shelf.color = color;
-             // For dark colors, use a gray background.
-             var bg = luminence(color) > 0.1 ? '#000' : '#666';
-             $('#model').css({'background-color': bg});
-             updateAnimation(shelf);
-           }
-           // Rate limit model updates since the color picker spews events rapidly
-           // and can cause slowness and/or webgl crashes.
-           setTimeout(function() {requestAnimFrame(checkColor);}, 1000);
-         }
-         checkColor();
      });"
+
+;     "jQuery(document).ready(function() {
+;         // Hookup the form controls.
+;         $('#colorpicker').farbtastic('#color');
+;         $('.chzn-select').chosen();
+;         $('.customStyleSelectBox').customSelect();
+;
+;         // Hookup on change events to update the model.
+;         $('#width').chosen().change(function() {
+;           shelf.width = $(this).val();
+;           updateAnimation(shelf);
+;         });
+;         $('#depth').chosen().change(function() {
+;           shelf.depth = $(this).val();
+;           updateAnimation(shelf);
+;         });
+;         $('#quantity').chosen().change(function() {
+;           shelf.quantity = $(this).val();
+;           updateAnimation(shelf);
+;         });
+;
+;         // Set the container div height to match the width.
+;         var model = $('#model');
+;         var height = model.width();
+;         model.css({height: height.toString() + 'px'});
+;
+;         // Start animating.
+;         drawShelf(shelf, model[0]);
+;         startAnimation();
+;
+;         // The color picker doesn't have a way to supply a callback in addition
+;         // to the synched form field. So, periodically check the background color
+;         // of the synched form field and re-render when it changes.
+;         function checkColor() {
+;           var color = $('#color').css('background-color');
+;           color = rgb2hex(color);
+;           if (shelf.color != color) {
+;             shelf.color = color;
+;             // For dark colors, use a gray background.
+;             var bg = luminence(color) > 0.1 ? '#000' : '#666';
+;             $('#model').css({'background-color': bg});
+;             updateAnimation(shelf);
+;           }
+;           // Rate limit model updates since the color picker spews events rapidly
+;           // and can cause slowness and/or webgl crashes.
+;           setTimeout(function() {requestAnimFrame(checkColor);}, 1000);
+;         }
+;         checkColor();
+;     });"
      ]]
    ])
 
