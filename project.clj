bf91acb4795b4118789dfbf6f5dce65f1bba7d60
Jonathan Millett <jon@sistemimoderni.com>
2013-09-27 11:24:34 -0300

    Updated all dependencies to latest versions.

--- project.clj
+++ project.clj
@@ -20,59 +20,58 @@
 
                  ;; logging
                  [clj-logging-config "1.9.10"]
-                 [org.slf4j/slf4j-api "1.7.2"]
-                 [org.slf4j/slf4j-log4j12 "1.7.2"]
+                 [org.slf4j/slf4j-api "1.7.5"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]
                  [log4j "1.2.17"]
 
-                 ;; for internationalized urls
-                 [com.google.gdata/core "1.47.1"]
-
                  ;; ring core
-                 [ring/ring-core "1.1.8"]
-                 [ring/ring-devel "1.1.8" :exclusions [hiccup]]
-                 [ring/ring-jetty-adapter "1.1.8"]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-devel "1.2.0" :exclusions [hiccup]]
+                 [ring/ring-jetty-adapter "1.2.0"]
 
                  ;; ring handlers and middleware
                  [net.cgrand/moustache "1.1.0"]
                  [ring-persistent-cookies "0.1.0"]
 
                  ;; html
-                 [hiccup "1.0.3"]
-                 [sistemi/gdata-core-minimal "1.0"] ; CUSTOM: stripped down gdata client
+                 [hiccup "1.0.4"]
 
                  ;; web client
-                 [clj-http "0.6.4"]
+                 [clj-http "0.7.7"]
 
                  ;; email
-                 [com.draines/postal "1.9.2"]
+                 [com.draines/postal "1.11.0"]
 
                  ;; configuration
                  [fidjet "0.0.2"]
 
                  ;; user agent detection
-                 [bitwalker/UserAgentUtils "1.8"]
+                 [bitwalker/UserAgentUtils "1.10"]
                  
                  ;; calculation
                  [dgraph "1.2.2"]
-                 [frinj "0.1.4"]
+                 [frinj "0.2.5"]
 
                  ;; payment
-                 [com.braintreepayments.gateway/braintree-java "2.24.0"]
+                 [com.braintreepayments.gateway/braintree-java "2.24.1"]
 
                  ;; misc
-                 [org.clojure/core.memoize "0.5.3"]
-                 ;;[org.clojure/core.cache "0.6.3"]
+                 [org.clojure/core.memoize "0.5.6"]
+
                  [ordered "1.3.2"]
-                 [com.google.guava/guava "13.0.1"]
-                 [org.clojure/tools.namespace "0.2.2"]
+                 [com.google.guava/guava "15.0"]
+                 [org.clojure/tools.namespace "0.2.4"]
 
                  ;; cljs
-                 [jayq "2.3.0"]            ;; jquery wrapper
-                 [rm-hull/monet "0.1.7"]   ;; html5 canvas
-                 [prismatic/dommy "0.1.1"] ;; jquery replacement using clojurescript idioms
+                 [jayq "2.4.0"]            ;; jquery wrapper
+                 [rm-hull/monet "0.1.8"]   ;; html5 canvas
+                 [prismatic/dommy "0.1.2"] ;; jquery replacement using clojurescript idioms
+
+                 ;; for internationalized urls
+                 [com.google.gdata/core "1.47.1"]
                  ]
 
-  :plugins [[lein-cljsbuild "0.3.0"]
+  :plugins [[lein-cljsbuild "0.3.3"]
             [lein-marginalia "0.7.1"]
             [lein-ancient "0.4.4"]
             ;; [lein-cloverage "1.0.2"] ; runs but no output is produced
@@ -96,6 +95,6 @@
                        :output-to "www/raw/js/main.min.js"
                        :output-dir "var/target/cljsbuild/prod"}}}}
 
-  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.3"]
-                                  [org.clojure/tools.nrepl "0.2.1"]
-                                  [ring-mock "0.1.2" :exclusions [org.clojure/clojure]]]}})
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
+                                  [org.clojure/tools.nrepl "0.2.3"]
+                                  [ring-mock "0.1.5" :exclusions [org.clojure/clojure]]]}})
