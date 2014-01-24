1c8a6672256f898b4cb04bcf61e6bdf3e33d424d
sistemidave <dave@sistemimoderni.com>
2014-01-23 20:03:07 -0600

    transact correctly handles 2 different error responses

--- src/clj/shipping/ups/transact.clj
+++ src/clj/shipping/ups/transact.clj
@@ -1,6 +1,7 @@
 (ns shipping.ups.transact
   (:require [clj-http.client :as client]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [shipping.ups.response :as rsp]))
 
 
 ;; Test sandbox url
@@ -12,6 +13,7 @@
   "Use Customer, Shelving, and Sistemi information to build the
   2 part UPS shipping request (ShippingConfirm, ShippingAccept)"
   [raw_xml_data]
-  (let [ship_confirm_rsp (client/post shipping-confirm {:content raw_xml_data})]
+  (let [ship_confirm_raw (client/post shipping-confirm {:body raw_xml_data})
+        ship_confirm_rsp (rsp/get-shipment-confirm-response (ship_confirm_raw :body))]
     ship_confirm_rsp
     ) )
