cad17a060f67ea001fd3a07b7593cc4570f02aaa
Jonathan Millett <jon@sistemimoderni.com>
2014-02-14 13:43:35 -0300

    analytics - disable/enable google analytics via configuration option.

--- src/clj/analytics/google.clj
+++ src/clj/analytics/google.clj
@@ -5,10 +5,11 @@
 ;; Note: modified to always use https
 (defn analytics
   "Inserts google analytics script"
-  [account]
-  [:script {:type "text/javascript"}
-   (format
-    "var _gaq = _gaq || [];
+  [{:keys [enabled? account]}]
+  (if enabled?
+    [:script {:type "text/javascript"}
+     (format
+      "var _gaq = _gaq || [];
   _gaq.push(['_setAccount', '%s']);
   _gaq.push(['_trackPageview']);
 
@@ -16,4 +17,4 @@
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = 'https://ssl.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();" account)])
+  })();" account)]))
