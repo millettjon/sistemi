b026072f85d1ef63bf5990710f5ae35c3a8ef753
Jonathan Millett <jon@sistemimoderni.com>
2014-06-28 22:23:12 -0400

    blog_test.clj - fixed typo.

--- test/clj/sistemi/blog_test.clj
+++ test/clj/sistemi/blog_test.clj
@@ -3,14 +3,14 @@
             [www.url :as u])
   (:use clojure.test))
 
-(deftest url-blog->site
-  (are [x y] (= (-> x b/url-blog->site str) y)
+(deftest url-from-blog-to-site
+  (are [x y] (= (-> x b/url-from-blog-to-site str) y)
        "https://blog.sm1.in/?p=31"                    "/en/blog?p=31"
        "https://blog.sm1.in/?p=31&lang=fr"            "/fr/blog?p=31"
        "https://blog.sm1.in/au-commencement/?lang=fr" "/fr/blog/au-commencement"))
 
 (deftest url-site->blog
-  (are [w x y z] (= (-> {:locale w :uri x :params y} b/url-site->blog) (u/new-URL z))
+  (are [w x y z] (= (-> {:locale w :uri x :params y} b/url-from-blog-to-site) (u/new-URL z))
        :en "/blog" {:p "31"}      "https://blog.sm1.in?lang=en&p=31"
        :fr "/blog" {:p "31"}      "https://blog.sm1.in?lang=fr&p=31"
        :en "/blog/3d-printing" {} "https://blog.sm1.in/3d-printing?lang=en"
