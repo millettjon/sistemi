555392b57d69aeedfbccaf59ca9a17a115022f92
Jonathan Millett <jon@sistemimoderni.com>
2014-12-08 09:08:20 -0300

    edn - Added slurp function.

--- src/clj/util/edn.clj
+++ src/clj/util/edn.clj
@@ -1,14 +1,21 @@
 (ns util.edn
   (:require [util.frinj :as f]
-            [clojure.edn :as edn])
+            [clojure.edn :as edn]
+            [util.path :as p])
   (:use ordered.map)
-  (:refer-clojure :exclude [read-string]))
+  (:refer-clojure :exclude [read-string slurp]))
 
 (def read-opts
   {:readers (merge f/readers
                    {'ordered/map ordered.map/ordered-map})})
 
 (defn read-string
-  "Like"
+  "Calls clojure.edn/read-string with custom options."
   [s]
   (edn/read-string read-opts s))
+
+(defn slurp
+  [file]
+  (let [f (p/new-path file)]
+    (if (p/exists? f)
+      (-> f p/to-file clojure.core/slurp read-string))))
