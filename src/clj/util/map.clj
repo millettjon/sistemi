81aa0ab034f05fc4f5cb46c52550bd9694f2d8b4
Jonathan Millett <jon@sistemimoderni.com>
2014-12-08 11:01:44 -0500

    map - added deep-merge.

--- src/clj/util/map.clj
+++ src/clj/util/map.clj
@@ -1,5 +1,19 @@
-(ns util.map) 
+(ns util.map
+  (:use [clojure.contrib.map-utils :only (deep-merge-with)]))
 
 (defn map-vals [f m]
   "Maps function f over the values of map m."
   (into {} (for [[k v] m] [k (f v)])))
+
+(defn deep-merge [& maps]
+  (->> maps
+       (remove nil?)
+       (apply deep-merge-with (fn [& l] (last l)))))
+
+#_ (let [a {:params {:foo :bar}}
+         b {:params {:baz :qux}}]
+     (deep-merge a b))
+
+#_ (let [a {:params {:foo :bar}}
+         b nil]
+     (deep-merge a b))
