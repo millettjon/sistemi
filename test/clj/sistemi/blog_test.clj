c1123dc6d2b614ea3e3bbeb77e8c304e802a1cc7
Jonathan Millett <jon@sistemimoderni.com>
2015-12-06 00:07:17 -0300

    blog-test - fixed broken tests.

--- test/clj/sistemi/blog_test.clj
+++ test/clj/sistemi/blog_test.clj
@@ -10,8 +10,8 @@
        "https://blog.sm1.in/fr/blog/au-commencement"  "/fr/blog/au-commencement"))
 
 (deftest url-site->blog
-  (are [w x y z] (= (-> {:locale w :uri x :params y} b/url-from-blog-to-site) (u/new-URL z))
-       :en "/blog" {:p "31"}      "https://blog.sm1.in?lang=en&p=31"
-       :fr "/blog" {:p "31"}      "https://blog.sm1.in?lang=fr&p=31"
-       :en "/blog/3d-printing" {} "https://blog.sm1.in/3d-printing?lang=en"
-       :fr "/blog/au-commencement" {} "https://blog.sm1.in/au-commencement?lang=fr"))
+  (are [w x y z] (= (b/url-from-site-to-blog {:locale w :uri x :params y}) (u/new-URL z))
+       "en" "/blog" {:p "31"}      "https://blog.sm1.in/en/blog?p=31"
+       "fr" "/blog" {:p "31"}      "https://blog.sm1.in/fr/blog?p=31"
+       "en" "/blog/3d-printing" nil "https://blog.sm1.in/en/blog/3d-printing"
+       "fr" "/blog/au-commencement" nil "https://blog.sm1.in/fr/blog/au-commencement"))
